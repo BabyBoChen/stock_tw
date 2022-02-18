e787eeeb7eda02be40f871c359f1a93801d7156c
BabyBoChen <35010886+BabyBoChen@users.noreply.github.com>
2022-02-19 01:37:44 +0800

    update data

--- get_daily_trans.py
+++ get_daily_trans.py
@@ -103,8 +103,8 @@ def get_daily_stock_data(target_date:datetime.date, stock_type:int) -> Optional[
 
 def main() -> None:
 
-    start = datetime.date(2022,1,27)
-    end = datetime.date(2022,2,13)
+    start = datetime.date(2022,2,14)
+    end = datetime.date(2022,2,18)
     for_db = []
 
     while start <= end:
